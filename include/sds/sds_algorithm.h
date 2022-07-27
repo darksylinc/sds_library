7e04bce0f4d3f179a8fd97e059c985c55fd56ebd
Matias N. Goldberg <dark_sylinc@yahoo.com.ar>
2022-07-27 14:28:41 -0300

    Add sds::bit_cast

--- include/sds/sds_algorithm.h
+++ include/sds/sds_algorithm.h
@@ -22,4 +22,30 @@ namespace sds
 			  ( s2 - s1  > (max >> 1u) ) );
 		// clang-format on
 	}
+
+	/// Performs the same as std::bit_cast
+	/// i.e. the same as reinterpret_cast but without breaking strict aliasing rules
+	template <class Dest, class Source>
+#if defined __has_builtin && __has_builtin( __builtin_bit_cast )
+	constexpr
+#else
+	inline
+#endif
+		Dest
+		bit_cast( const Source &source )
+	{
+#if defined __has_builtin && __has_builtin( __builtin_bit_cast )
+		return __builtin_bit_cast( Dest, source );
+#else
+		static_assert( sizeof( Dest ) == sizeof( Source ),
+					   "bit_cast requires source and destination to be the same size" );
+		static_assert( std::is_trivially_copyable<Dest>::value,
+					   "bit_cast requires the destination type to be copyable" );
+		static_assert( std::is_trivially_copyable<Source>::value,
+					   "bit_cast requires the source type to be copyable" );
+		Dest dest;
+		memcpy( &dest, &source, sizeof( dest ) );
+		return dest;
+#endif
+	}
 }  // namespace sds
